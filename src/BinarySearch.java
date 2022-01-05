6783f48fd74230af4ca176b1084f30d6ef84b668
abhishek korde <abhishekkorde44@gmail.com>
2022-01-05 20:14:20 +0530

    binary search - user input added to key

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,11 +1,15 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class BinarySearch {
     public static void main(String[] args) {
         int[] arr = {12, 45, 78, 20, 1, 64, 1, 47, 45, 4, 20};
+        Scanner in = new Scanner(System.in);
+        System.out.println("enter the key : ");
+        int key = in.nextInt();
         Arrays.sort(arr);
-        int index = binarySearch(arr, 78);
-        System.out.println((index < 0) ? "key not found" : "key found at index " + index);
+        int index = binarySearch(arr,key);
+        System.out.println((index < 0) ? "key not found" : key + " found at index " + index);
     }
 
     static int binarySearch(int[] arr, int key) {
